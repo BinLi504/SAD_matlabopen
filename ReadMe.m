97989a75b61439b2a9d019a9ca0dd9a7e738184a
BinLi504 <72616868+BinLi504@users.noreply.github.com>
2020-12-06 13:59:49 +0800

    Add files via upload

---
+++ ReadMe.m
@@ -0,0 +1,34 @@
+% The folder "SADdemo" contains the codes for the following four
+% sub-folders:
+
+% Core_files: contains the implementation of the SAD algorithm
+
+% Sample_test: contains one example on how to use the SAD
+% algorithm with the least square method to process data in batches. 
+
+% Dataset: contains the signals tested in the paper referenced below 
+
+% otherAlgorithms: contains the ASimCo, ASPADE and ConstDL angorithms.
+
+% Our codes run in Matlab R2019b (9.7.0.1190202 (R2019b)), and use the toolbox of Neural Network, 
+% Communications, Audio and Optimization and Signal Processing.
+%
+% References:
+
+% B. Li, L. Rencker, J. Dong, Y. Luo, M.D. Plumbley, W. Wang
+% "Sparse Analysis Model Based Dictionary Learning for Signal Declipping"
+% submitted to IEEE Journal of Selected Topics in Signal Processing, January 2021.
+% 
+%
+% Written by Bin Li, moderated by Wenwu Wang, version 1.0                                    
+%
+% If you have any questions or comments regarding this package, or if you want to 
+% report any bugs or unexpected error messages, please send an e-mail to
+% w.wang@surrey.ac.uk
+%     
+% Copyright 2020 B. Li, L. Rencker, J. Dong, Y. Luo, M.D. Plumbley, W. Wang
+% 
+% This software is a free software distributed under the terms of the GNU 
+% Public License version 3 (http://www.gnu.org/licenses/gpl.txt). You can 
+% redistribute it and/or modify it under the terms of this licence, for 
+% personal and non-commercial use and research purpose.
